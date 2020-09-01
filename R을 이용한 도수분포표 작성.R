162a20d4388698a8f94cfb4ad32409a2fc0b4ecd
esctabcapslock <62400002+esctabcapslock@users.noreply.github.com>
2020-09-01 12:23:35 +0000

    Create R을 이용한 도수분포표 작성.R

---
+++ R을 이용한 도수분포표 작성.R
@@ -0,0 +1,14 @@
+액셀<-read.csv("res1.csv",header=T);
+x=액셀$저항;
+n=length(x);
+hx=hist(x,breaks=seq(min(x),max(x),by=(max(x)-min(x))/20),right=F);
+
+xcf<-cumsum(hx$counts)
+xrf<-hx$counts/n#상대도수
+xrcf<-xcf/n; #상대누적도수;
+xtable<-cbind(round(hx$mid,3),hx$counts,xcf,xrf,xrcf);
+
+xclass<-paste0('[', round(hx$breaks[-length(hx$breaks)],3),',',round(hx$breaks[-1],3),')');
+rownames(xtable)<-xclass;
+colnames(xtable)<-c('대푯값','도수','누적도수','상대도수','상대누적도수')
+xtable
